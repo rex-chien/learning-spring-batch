b085cfbe0a06bad8d174ff7fa5e16c775af262d4
rex.chien <rex.chien.0509@gmail.com>
2018-01-02 18:46:41 +0800

    Splits Jobs Example

---
+++ splits/src/test/java/rex/chien/learning/springbatch/splits/SplitsApplicationTests.java
@@ -0,0 +1,16 @@
+package rex.chien.learning.springbatch.splits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SplitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
