80ba3aefc44a8c18eee1198b15d7bb2728dd0a22
rex.chien <rex.chien.0509@gmail.com>
2018-01-02 18:36:28 +0800

    Transitions Job Example

---
+++ transitions/src/main/java/rex/chien/learning/springbatch/transitions/TransitionsApplication.java
@@ -0,0 +1,12 @@
+package rex.chien.learning.springbatch.transitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitionsApplication.class, args);
+	}
+}
