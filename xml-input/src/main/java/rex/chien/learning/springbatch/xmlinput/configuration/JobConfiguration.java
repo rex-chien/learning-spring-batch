7f4e07ba7fd992036ec1ff60bbfdb209e1d2c2d0
rex.chien <rex.chien.0509@gmail.com>
2018-01-04 16:07:27 +0800

    Xml Input Example

---
+++ xml-input/src/main/java/rex/chien/learning/springbatch/xmlinput/configuration/JobConfiguration.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rex.chien.learning.springbatch.xmlinput.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import rex.chien.learning.springbatch.xmlinput.domain.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Rex Chien
+ */
+@Configuration
+public class JobConfiguration {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public StaxEventItemReader<Customer> customerItemReader() {
+
+		XStreamMarshaller unmarshaller = new XStreamMarshaller();
+
+		Map<String, Class> aliases = new HashMap<>();
+		aliases.put("customer", Customer.class);
+
+		unmarshaller.setAliases(aliases);
+
+		StaxEventItemReader<Customer> reader = new StaxEventItemReader<>();
+
+		reader.setResource(new ClassPathResource("/data/customers.xml"));
+		reader.setFragmentRootElementName("customer");
+		reader.setUnmarshaller(unmarshaller);
+
+		return reader;
+	}
+
+	@Bean
+	public ItemWriter<Customer> customerItemWriter() {
+		return items -> {
+			for (Customer item : items) {
+				System.out.println(item.toString());
+			}
+		};
+	}
+
+	@Bean
+	public Step step1() {
+		return stepBuilderFactory.get("step1")
+				.<Customer, Customer>chunk(10)
+				.reader(customerItemReader())
+				.writer(customerItemWriter())
+				.build();
+	}
+
+	@Bean
+	public Job job() {
+		return jobBuilderFactory.get("job")
+				.start(step1())
+				.build();
+	}
+}
---
+++ xml-input/src/test/java/rex/chien/learning/springbatch/xmlinput/XmlInputApplicationTests.java
@@ -0,0 +1,16 @@
+package rex.chien.learning.springbatch.xmlinput;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XmlInputApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
