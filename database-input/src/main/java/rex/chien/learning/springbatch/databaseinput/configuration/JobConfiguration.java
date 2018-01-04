40896eff55328886f822f261cfd6eb45e1d83663
rex.chien <rex.chien.0509@gmail.com>
2018-01-04 11:17:42 +0800

    Database Input Example

---
+++ database-input/src/main/java/rex/chien/learning/springbatch/databaseinput/configuration/JobConfiguration.java
@@ -0,0 +1,94 @@
+package rex.chien.learning.springbatch.databaseinput.configuration;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import rex.chien.learning.springbatch.databaseinput.domain.Customer;
+import rex.chien.learning.springbatch.databaseinput.domain.CustomerRowMapper;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Rex Chien
+ */
+@Configuration
+public class JobConfiguration {
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    public DataSource dataSource;
+//
+//	@Bean
+//	public JdbcCursorItemReader<Customer> cursorItemReader() {
+//		JdbcCursorItemReader<Customer> reader = new JdbcCursorItemReader<>();
+//
+//		reader.setSql("select id, firstName, lastName, birthdate from customer order by lastName, firstName");
+//		reader.setDataSource(this.dataSource);
+//		reader.setRowMapper(new CustomerRowMapper());
+//
+//		return reader;
+//	}
+
+    @Bean
+    public JdbcPagingItemReader<Customer> pagingItemReader() {
+        JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+        reader.setDataSource(this.dataSource);
+        reader.setFetchSize(10);
+        reader.setRowMapper(new CustomerRowMapper());
+
+        MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+        queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+        queryProvider.setFromClause("from customer");
+
+        Map<String, Order> sortKeys = new HashMap<>(1);
+
+        sortKeys.put("id", Order.ASCENDING);
+
+        queryProvider.setSortKeys(sortKeys);
+
+        reader.setQueryProvider(queryProvider);
+
+        return reader;
+    }
+
+    @Bean
+    public ItemWriter<Customer> customerItemWriter() {
+        return items -> {
+            for (Customer item : items) {
+                System.out.println(item.toString());
+            }
+        };
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(10)
+                .reader(pagingItemReader())
+                .writer(customerItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Job job() {
+        return jobBuilderFactory.get("job")
+                .start(step1())
+                .build();
+    }
+
+}
---
+++ database-input/src/test/java/rex/chien/learning/springbatch/databaseinput/DatabaseInputApplicationTests.java
@@ -0,0 +1,16 @@
+package rex.chien.learning.springbatch.databaseinput;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseInputApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
